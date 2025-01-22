728bbd21bd68d391e9d4e9c2d96013139081143e
sanji <sanji@gmail.com>
2025-01-22 12:06:04 +0000

    tweaked some lines

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   // this is just a comment for practice
-  return <div>now this is the current page that you will see on the page</div>;
+  return (
+    <div>
+      now this is the current page that you will see on the page
+      <p>And i tweaked some changes here </p>
+    </div>
+  );
   // here is another comment that i am adding
 }
 
