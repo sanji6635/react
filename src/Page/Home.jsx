51d6cac375bbe6bb8aa237b97f37c6199ce7bc3c
sanji <sanji@gmail.com>
2025-01-23 23:26:59 +0000

    some changes to see

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div> this is the homepage </div>;
+  return (
+    <div>
+      <h1>this is the homepage</h1>{" "}
+    </div>
+  );
 };
 
 export default Home;
