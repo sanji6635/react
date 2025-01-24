1243b5dcd723171d11eebc2c12612d9e4b328b84
sanji <sanji@gmail.com>
2025-01-24 15:22:56 +0000

    added some extra files

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-
+import Navbar from "../components/navbar/Navbar";
 const Home = () => {
   return (
-    <div>
-      <h1>this is updated Homepage</h1>
-      <h2> that means this hashrouter works</h2>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
