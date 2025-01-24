fd61844522d21e7392a8793b2df360d5e8b1b997
sanji <sanji@gmail.com>
2025-01-24 00:16:44 +0000

    changed from browser router to hash Router

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>this is updated Homepage</h1>{" "}
+      <h1>this is updated Homepage</h1>
+      <h2> that means this hashrouter works</h2>
     </div>
   );
 };
